1c59e6dfb1491f153f2ccd2ca8634c9eded4b637
simo8986 <simo8986@student.eadania.dk>
2018-11-23 13:57:06 +0100

    23/11/2018
    Opdateret

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var x = document.getElementById("menupunkter");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
